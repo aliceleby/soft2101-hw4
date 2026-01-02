77958e7122fbdefa112c25096dc8a7cead136626
Ali Çelebi <22comp1010@isik.edu.tr>
2026-01-02 12:22:27 +0300

    satisfying requirement one

---
+++ hmk1/RangePrinter.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class RangePrinter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first integer: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter second integer: ");
+        int b = scanner.nextInt();
+
+        if (a < b) {
+            for (int i = a + 1; i < b; i++) {
+                System.out.println(i);
+            }
+        } else if (a > b) {
+            for (int i = a - 1; i > b; i--) {
+                System.out.println(i);
+            }
+        } else {
+            System.out.println("No integers between the given numbers.");
+        }
+
+        scanner.close();
+    }
+}
